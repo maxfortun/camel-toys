d0d91668d3f266112263903b5d508e9d10947fbe
Max <max@maxf.net>
2025-04-01 22:30:20 -0400

    updated

---
+++ src/org/apache/camel/support/processor/FilteringExchangeFormatter.java
@@ -0,0 +1,90 @@
+package org.apache.camel.support.processor;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import java.util.regex.Pattern;
+
+import org.apache.camel.Exchange;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class FilteringExchangeFormatter extends DefaultExchangeFormatter {
+	private static final Logger logger = LogManager.getLogger(FilteringExchangeFormatter.class);
+
+	private String keyFilterPattern = null;
+	private String valueFilterPattern = null;
+	private String valueTypeFilterPattern = null;
+
+	public void setKeyFilterPattern(String keyFilterPattern) {
+		this.keyFilterPattern = keyFilterPattern;
+	}
+
+	public String getKeyFilterPattern() {
+		return keyFilterPattern;
+	}
+
+	public void setValueFilterPattern(String valueFilterPattern) {
+		this.valueFilterPattern = valueFilterPattern;
+	}
+
+	public String getValueFilterPattern() {
+		return valueFilterPattern;
+	}
+
+	public void setValueTypeFilterPattern(String valueTypeFilterPattern) {
+		this.valueTypeFilterPattern = valueTypeFilterPattern;
+	}
+
+	public String getValueTypeFilterPattern() {
+		return valueTypeFilterPattern;
+	}
+
+	protected Map<String, Object> filterHeaderAndProperties(Map<String, Object> map) {
+		if(
+			   null == keyFilterPattern
+			&& null == valueTypeFilterPattern
+			&& null == valueFilterPattern
+		) {
+			logger.trace("No filter patterns set.");
+			return map;
+		}
+
+		return map.entrySet()
+			.stream()
+			.filter(entry -> {
+
+				String key = entry.getKey();
+
+				if(null != keyFilterPattern && key.matches(keyFilterPattern)) {
+					logger.trace("{} matches keyFilterPattern {}.", key, keyFilterPattern);
+					return false;
+				}
+
+				Object value = entry.getValue();
+
+				if(null != valueTypeFilterPattern) {
+					String valueType = value.getClass().getName();
+					if(valueType.matches(valueTypeFilterPattern)) {
+						logger.trace("{} matches valueTypeFilterPattern {}.", valueType, valueTypeFilterPattern);
+						return false;
+					}
+				}
+
+				if(null != valueFilterPattern) {
+					String stringValue = (null != value ? value.toString() : "");
+					if(stringValue.matches(valueFilterPattern)) {
+						logger.trace("{} matches valueFilterPattern {}.", value, valueFilterPattern);
+						return false;
+					}
+				}
+
+				logger.trace("{} ok to trace.", key);
+
+				return true;
+			})
+			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+	}
+}
+
