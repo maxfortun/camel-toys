e3a17abde831520eafb1c7ac974833658e83d7c7
Max <max@maxf.net>
2025-04-03 18:58:06 -0400

    updated

---
+++ src/main/java/net/maxf/apache/camel/SyncAsyncBridge.java
@@ -0,0 +1,21 @@
+package net.maxf.apache.camel;
+
+import java.util.Map;
+
+import org.apache.camel.Exchange;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class SyncAsyncBridge {
+	public void queueForReply(Exchange exchange) {
+	}
+
+	public void waitForReply(Exchange exchange) {
+	}
+
+	public void notifyOfReply(Exchange exchange) {
+	}
+
+}
+
