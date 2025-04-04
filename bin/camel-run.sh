7989eb4d51a62b95fd966130eea5be4ddd8d6f0f
Max <max@maxf.net>
2025-04-04 00:28:40 -0400

    updated

---
+++ bin/camel-run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+camelContext=$1
+
+if [ -z "$camelContext" ]; then
+	echo "$0: <camel-context.xml>"
+	exit 1
+fi
+
+export NODE_ID=a
+
+java -cp "src/main/resources:target/*:target/dependency/*" \
+	org.apache.camel.spring.Main \
+	-ac $camelContext
