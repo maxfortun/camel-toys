90bf1233de31dd64399197faa1b61c7faf29a00c
Max <max@maxf.net>
2025-04-04 11:29:27 -0400

    updated

---
+++ bin/setenv-syncasync-kafka.sh
@@ -0,0 +1,11 @@
+
+export KAFKA_BROKERS=${KAFKA_BROKERS:-localhost:9092}
+
+export PRODUCER_START="kafka:syncAsyncGate-start?brokers=$KAFKA_BROKERS"
+export CONSUMER_START="kafka:syncAsyncGate-start?brokers=$KAFKA_BROKERS"
+
+export PRODUCER_DONE="kafka:syncAsyncGate-done?brokers=$KAFKA_BROKERS"
+export CONSUMER_DONE="kafka:syncAsyncGate-done?brokers=$KAFKA_BROKERS"
+
+export PRODUCER_REPLY_TO="kafka:syncAsyncGate-replyTo?brokers=$KAFKA_BROKERS"
+export CONSUMER_REPLY_TO="kafka:syncAsyncGate-replyTo?brokers=$KAFKA_BROKERS"
