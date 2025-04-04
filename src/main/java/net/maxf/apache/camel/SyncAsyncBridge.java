3c3ca22013fcb4a939cc7de1ea566ee4a801e8f4
Max <max@maxf.net>
2025-04-04 01:39:57 -0400

    updated

--- src/main/java/net/maxf/apache/camel/SyncAsyncBridge.java
+++ src/main/java/net/maxf/apache/camel/SyncAsyncBridge.java
@@ -1,6 +1,7 @@
 package net.maxf.apache.camel;
 
 import java.util.Map;
+import java.util.HashMap;
 
 import org.apache.camel.Exchange;
 
@@ -8,14 +9,27 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 public class SyncAsyncBridge {
-	public void queueForReply(Exchange exchange) {
+	private static final Logger logger = LogManager.getLogger(SyncAsyncBridge.class);
+
+	private Map<String, Exchange> requests = new HashMap<>();
+
+	public void queueForReply(Exchange request) {
+		logger.debug(request.getExchangeId());
+		synchronized(request) {
+			requests.put(request.getExchangeId(), request);
+		}
 	}
 
-	public void waitForReply(Exchange exchange) {
+	public void waitForReply(Exchange request, long timeout) throws InterruptedException {
+		logger.debug(request.getExchangeId());
+		synchronized(request) {
+			request.wait(timeout);
+		}
 	}
 
-	public void notifyOfReply(Exchange exchange) {
+	public void notifyOfReply(Exchange response) {
+		logger.debug(response.getExchangeId());
+		// Exchange request = null;
 	}
-
 }
 
